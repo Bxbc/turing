f3fd02bbb3da4dfa2936e40dd28ccc15490f3760
bixi <bixi.bx@alibaba-inc.com>
2025-09-16 15:50:48 +0800

    init

---
+++ turing-adaptor/src/test/java/com/ai/turing/adaptor/TuringAdaptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.turing.adaptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringAdaptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ turing-start/src/main/java/com/ai/turing/start/TuringStartApplication.java
@@ -0,0 +1,13 @@
+package com.ai.turing.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringStartApplication.class, args);
+    }
+
+}
